5572fe11ee4c1cca5f493a5b1b9a5e003adc9293
mountainricardo <ricardoserrano@grupotecopy.es>
2022-02-05 19:45:04 +0100

    Set Express server on port 8000 to search for itunes music albums

--- server/itunes-service.js
+++ server/itunes-service.js
@@ -4,20 +4,16 @@ const axios = require('axios');
 const router = express.Router();
 
 const BASE_URL = 'https://itunes.apple.com';
-const queryType = '/search/'
+const queryType = 'search'
 
 router.get('/albums', async (req, res, next) => {
-  const url = new URL(`${ BASE_URL }${queryType}${req.url}`)
+  const url = new URL(`${ BASE_URL }/${queryType}/${req.url}`)
   const queryTerm = url.searchParams.get('term') ? `?term=${url.searchParams.get('term')}` : '';
   const queryAlbum = url.searchParams.get('entity') ? `&entity=${ url.searchParams.get('entity') }` : '';
-  const queryUrl = `${ BASE_URL }${queryType}${queryTerm}${queryAlbum}`
+  const queryUrl = `${ BASE_URL }/${queryType}/${queryTerm}${queryAlbum}`
 
   try {
-      // const {data} = await axios.get(BASE_URL);
       const {data} = await axios.get(queryUrl);
-      cacheData = data;
-      cacheTime = Date.now();
-      data.cacheTime = cacheTime;
       return res.json(data);
    } catch(err) {
       return next(err);
